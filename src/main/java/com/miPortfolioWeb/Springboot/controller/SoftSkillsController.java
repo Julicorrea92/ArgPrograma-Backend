f9cfee3d08ffa1d7a57c3c3429f7273c2dddaaee
Julian Correa <101025019+Julicorrea92@users.noreply.github.com>
2023-04-25 13:25:51 -0300

    Update SoftSkillsController.java

--- src/main/java/com/miPortfolioWeb/Springboot/controller/SoftSkillsController.java
+++ src/main/java/com/miPortfolioWeb/Springboot/controller/SoftSkillsController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://correajportfolio.web.app")
+@CrossOrigin(origins = "*")
 @RequestMapping("/softskill")
 public class SoftSkillsController {
     
